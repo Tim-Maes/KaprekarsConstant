0c0767b2a24506947cf71435ab565bd7cd5a9eec
Tim Maes <91606949+Tim-Maes@users.noreply.github.com>
2023-11-17 14:44:06 +0100

    Update program

--- KaprekarsConstant.Console/Program.cs
+++ KaprekarsConstant.Console/Program.cs
@@ -1,6 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Enter a 4-digit number:");
+﻿Console.WriteLine("Enter a 4-digit number:");
 string input = Console.ReadLine();
 
 if (!IsValidNumber(input))
